72f0ec475df1c389779a9105d493fc8837cc56cb
wahyufaturrizki <wahyufaturrizkyy@gmail.com>
2024-02-14 20:34:04 +0700

    FEAT: set up providers react query

---
+++ src/app/providers.tsx
@@ -0,0 +1,26 @@
+// app/providers.jsx
+"use client";
+
+import { useState, ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental";
+import * as React from "react";
+
+export function Providers(props: { children: ReactNode }) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 5 * 1000,
+          },
+        },
+      })
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryStreamedHydration>{props.children}</ReactQueryStreamedHydration>
+    </QueryClientProvider>
+  );
+}
